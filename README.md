fe3dd911482ff82b7e5ada33fdbd722c30989159
Louis Liao <buzonliao@gmail.com>
2023-08-26 18:24:48 +0800

    update

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
 # wdio-appium-v8
+
+- Install Node.js v16 
+    - suggest to use [NVM](https://github.com/nvm-sh/nvm)
+- Install JDK 
+    - `brew inistall openjdk@11`
+- Install Android Studio
+- setup `$JAVA_HOME` and `$ANDROID_HOME` as environment variables
+- Create Android Emulator and launch it
+- Install Appium Server
+    - `npm i --location=global appium`
+- Install Uiautomator2 driver
+    - `appium driver install uiautomator2`
+- Install Appium-Doctor to check the environment setup (optional) 
+    - ```js
+        npm install appium-doctor -g
+        appium-doctor
+        ```
+
+### Environment Setup of this Repository
+- Node.js v16.20.2
+- OpenJDK v11.0.20.0
+- Android Studio Giraffe | 2022.3.1 Patch 1Android Studio
+- Android Emulator Pixel 4, API 34, OS 14.0 
+- Appium Server v2.1.3
+- UiAutomator2 v2.29.5
+
+### Run Steps
+- Clone the repository
+- Install dependencies
+    - `npm i`
+- Launch Android Emulator
+- Launch Appium Server
+    - `appium`
+- Execute test
+    - `npx wdio`
+
+### Resource
+- https://www.udemy.com/course/appium-webdriverio-mobile-automation
--- test/specs/android-findElements.spec.js
+++ test/specs/android-findElements.spec.js
@@ -13,13 +13,14 @@ describe('Android Elements Tests', () => {
   
     it('Find element by class name', async () => {
       // find element by class name
+      // '$' returns the first element from list 
       const className = await $('android.widget.TextView');
   
       // Assertion
       await expect(className).toHaveText("API Demos");
     });
 
-    it('Find elements by Xpath', async () => {
+    xit('Find elements by Xpath', async () => {
       // xpath - (//tagname[@attribute=value])
       await $('//android.widget.TextView[@content-desc="Alert Dialogs"]').click();
   
@@ -34,46 +35,48 @@ describe('Android Elements Tests', () => {
       await expect(textAssertion).toHaveText("You selected: 1 , Command two");
     });
   
-    // it('Find elements by UIAutomator', async () => {
-    //   // find by text contains
-    //   await $('android=new UiSelector().textContains("Alert")').click();
-    // });
+    it('Find elements by UIAutomator', async () => {
+      // find by text contains
+      await $('android=new UiSelector().textContains("Alert")').click();
+    });
   
-    // it('Find multiple elements', async () => {
-    //   const expectedList = [
-    //     'API Demos', "Access'ibility",
-    //     'Accessibility', 'Animation',
-    //     'App', 'Content',
-    //     'Graphics', 'Media',
-    //     'NFC', 'OS',
-    //     'Preference', 'Text',
-    //     'Views'
-    //   ]
-    //   const actualList = []
+    it('Find multiple elements', async () => {
+      // '$' for single element
+      // '$$' for multiple elements
+      const expectedList = [
+        'API Demos', "Access'ibility",
+        'Accessibility', 'Animation',
+        'App', 'Content',
+        'Graphics', 'Media',
+        'NFC', 'OS',
+        'Preference', 'Text',
+        'Views'
+      ]
+      const actualList = []
   
-    //   // find multiple elements
-    //   const textList = await $$('android.widget.TextView');
+      // find multiple elements
+      const textList = await $$('android.widget.TextView');
+      
+      // loop through them
+      for (const element of textList) {
+        actualList.push(await element.getText());
+      }
   
-    //   // loop through them
-    //   for (const element of textList) {
-    //     actualList.push(await element.getText());
-    //   }
+      // assert the list
+      await expect(actualList).toEqual(expectedList);
+    });
   
-    //   // assert the list
-    //   await expect(actualList).toEqual(expectedList);
-    // });
+    it.only('Working with text field', async () => {
+      // access the auto complete screen
+      await $('~Views').click();
+      await $('//*[@text="Auto Complete"]').click();
+      await $('//*[@content-desc="1. Screen Top"]').click();
   
-    // it('Working with text field', async () => {
-    //   // access the auto complete screen
-    //   await $('~Views').click();
-    //   await $('//*[@text="Auto Complete"]').click();
-    //   await $('//*[@content-desc="1. Screen Top"]').click();
+      // enter the country name
+      const textField = await $('//*[@resource-id="io.appium.android.apis:id/edit"]');
+      await textField.addValue('Canada');
   
-    //   // enter the country name
-    //   const textField = await $('//*[@resource-id="io.appium.android.apis:id/edit"]');
-    //   await textField.addValue('Canada');
-  
-    //   // verify the country name
-    //   await expect(textField).toHaveText('Canada');
-    // });
+      // verify the country name
+      await expect(textField).toHaveText('Canada');
+    });
   });
